03d60cff417547bc6f862b05638e0597ba033cee
Roger Herrera <rogerleonardoh@gmail.com>
2020-10-23 12:56:42 -0500

    Implements base theme
    The radix theme is extended as the basis for the site theme

---
+++ web/themes/custom/multiplex/src/js/multiplex.script.js
@@ -0,0 +1,13 @@
+import 'popper.js';
+import 'bootstrap';
+
+(function () {
+
+  'use strict';
+
+  Drupal.behaviors.helloWorld = {
+    attach: function (context) {
+    }
+  };
+
+})(jQuery, Drupal);
